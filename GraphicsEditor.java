72ddd09a2dccb54103d4f67743874eae7038d8e3
towulik <82773555+towulik@users.noreply.github.com>
2021-07-26 21:49:41 +0300

    Create GraphicsEditor.java

---
+++ GraphicsEditor.java
@@ -0,0 +1,15 @@
+public class GraphicsEditor {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        Quad quad = new Quad();
+        Triangle triangle = new Triangle();
+        Rectangle rectangle = new Rectangle();
+        Trapezoid trapezoid = new Trapezoid();
+
+        circle.print();
+        quad.print();
+        triangle.print();
+        rectangle.print();
+        trapezoid.print();
+    }
+}
