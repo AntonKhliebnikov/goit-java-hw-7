97ad2d55447e3cc3013d05069f72a7dca984b27d
towulik <82773555+towulik@users.noreply.github.com>
2021-07-29 21:20:19 +0300

    Update Trapezoid.java

--- Trapezoid.java
+++ Trapezoid.java
@@ -1,11 +1,7 @@
 public class Trapezoid extends Shape implements ShapeActions {
-    @Override
-    public String getName() {
-        return "Trapezoid";
-    }
 
     @Override
-    public void print() {
-        System.out.println("This figure is " + getName());
+    public void getShapeName() {
+        System.out.println("Trapezoid");
     }
 }
