63b7540caed29e5daf3ee05ce2785fcd7435a39e
towulik <82773555+towulik@users.noreply.github.com>
2021-07-29 21:19:17 +0300

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,11 +1,7 @@
 public class Rectangle extends Shape implements ShapeActions {
+    
     @Override
-    public String getName() {
-        return "Rectangle";
-    }
-
-    @Override
-    public void print() {
-        System.out.println("This figure is " + getName());
+    public void getShapeName() {
+        System.out.println("Rectangle");
     }
 }
