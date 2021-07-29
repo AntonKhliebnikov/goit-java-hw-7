d6e46dbc5ae22561788e4711accd661902a5ca16
towulik <82773555+towulik@users.noreply.github.com>
2021-07-29 21:20:41 +0300

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -1,11 +1,7 @@
 public class Triangle extends Shape implements ShapeActions {
+    
     @Override
-    public String getName() {
-        return "Triangle";
-    }
-
-    @Override
-    public void print() {
-        System.out.println("This figure is " + getName());
+    public void getShapeName() {
+        System.out.println("Triangle");
     }
 }
