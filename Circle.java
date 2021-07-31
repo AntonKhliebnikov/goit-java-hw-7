c090366355fab6872f5cfad49a22f40bf4c28426
towulik <82773555+towulik@users.noreply.github.com>
2021-07-31 18:31:01 +0300

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,7 +1,14 @@
-public class Circle extends Shape implements ShapeActions {
+public class Circle extends Shape implements Perimeter {
+    private static final double Pi = 3.1415926536;
+    private double radius = 15;
 
     @Override
     public String getShapeName() {
         return "Circle";
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * Pi * radius;
     }
 }
