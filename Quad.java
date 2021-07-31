39c691ecc748846d44a872a74c956efd0a8843bc
towulik <82773555+towulik@users.noreply.github.com>
2021-07-31 18:33:06 +0300

    Update Quad.java

--- Quad.java
+++ Quad.java
@@ -1,7 +1,13 @@
-public class Quad extends Shape implements ShapeActions {
-    
+public class Quad extends Shape implements Perimeter {
+    private double side = 5;
+
     @Override
     public String getShapeName() {
         return "Quad";
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 4 * side;
     }
 }
