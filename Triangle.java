97fb19d44af8a4c67b214877e6a13603649529ec
towulik <82773555+towulik@users.noreply.github.com>
2021-07-31 18:35:23 +0300

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -1,7 +1,15 @@
-public class Triangle extends Shape implements ShapeActions {
+public class Triangle extends Shape implements Perimeter {
+    private double sideA = 5;
+    private double sideB = 8;
+    private double sideC = 10;
 
     @Override
     public String getShapeName() {
         return "Triangle";
+    }
+
+    @Override
+    public double getPerimeter() {
+        return sideA + sideB + sideC;
     }
 }
