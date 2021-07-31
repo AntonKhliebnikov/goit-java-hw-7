54c4218a65cc5a425e3e8b88194fbf96d0510276
towulik <82773555+towulik@users.noreply.github.com>
2021-07-31 18:35:03 +0300

    Update Trapezoid.java

--- Trapezoid.java
+++ Trapezoid.java
@@ -1,7 +1,16 @@
-public class Trapezoid extends Shape implements ShapeActions {
+public class Trapezoid extends Shape implements Perimeter {
+    private double sideA = 5;
+    private double sideB = 8;
+    private double sideC = 10;
+    private double sideD = 7;
 
     @Override
     public String getShapeName() {
         return "Trapezoid";
+    }
+
+    @Override
+    public double getPerimeter() {
+        return sideA + sideB + sideC + sideD;
     }
 }
