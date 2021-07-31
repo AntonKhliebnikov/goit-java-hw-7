a741eb15c7bac0b12b3f3315f7c2f4a2bd059c5e
towulik <82773555+towulik@users.noreply.github.com>
2021-07-31 18:33:23 +0300

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,7 +1,14 @@
-public class Rectangle extends Shape implements ShapeActions {
+public class Rectangle extends Shape implements Perimeter {
+    private double sideA = 5;
+    private double sideB = 10;
 
     @Override
     public String getShapeName() {
         return "Rectangle";
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * (sideA + sideB);
     }
 }
